806df57c0ba04aa86ecff2a6fc31dc566d9a56e9
Matheus Gonçalves De Britto <94484304+omatheusbritto@users.noreply.github.com>
2025-04-03 14:49:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
 # Sistema bancário
 ''' 
-Crie um sistema de banco que faça entrada e saida de valores de valores,
-com opções de Saque, Depósito e extratos.
-na opção saque não pode ser menor do que o saldo atual existente,
-limitato á 3 saque por dias e com o valor maxímo de R$500 por saque
-o depósito não pode ser valor negativo,
-o extrato deve-se mostrar todas as transações realizada 
+crie um menu principal com as opções de funções bancárias e internas
+
+Para a funções bancárias
+Crie um menu com as funções bancarias como Sacar, Depositar, extrato voltar ao menu principal
+
+RESTRIÇÕES!!!
+1º O usuario não pode fazer mais que 10 transações bancarias por dia
+(Caso o numero de transação seja atingida mostrar mensagem de erro)
+
+2º O usuario não pode sacar valor acima de R$ 500 por vez
+(Mostrar a mensagem de erro, valor não foi permitido por saque)
+
+3º O usuario pode sacar no maximo R$1500 por dia
+(Mostrar a mensagem de erro, valor de saque diario atingido)
+
+4º O usuario não poderá sacar valores negativo
+(Mostrar a mensagem de erro, valor não permitido)
+
+5º Na função deposito, não será permitido deposito com valores negativo
+(Mostrar a mensagem de erro, valor não permitido)
+
+6º Na função Saldo, mostrar o valor em conta
+( Mostrar saldo com data e horario em que foi solicitado)
+
+7º na função extrato, mostrar detalhadamente todas as transações
+( Mostrar se foi depositado ou sacado, valor, data e hora)
+
+Para funções externas
+Crie um menu com as funções de cadastrar novo usuario, listar usuarios e voltar ao menu principal
+
+1º Na função cadastrar novos usuarios
+validar com o CPF
+Caso CPF ja esteja cadastrado mostrar mensagem de erro (dados ja cadastrado)
+mostrar todos os dados do usuario que tenha esse CPF
+cadastrar nome, email, telefone e endereço
+
+2º criar a função de volTar ao menu principal
+
 '''
 from funcao import main
 

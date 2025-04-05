4313e87b26dc4a80a95cc942dbdacd906bc7bfb1
Matheus Gonçalves De Britto <94484304+omatheusbritto@users.noreply.github.com>
2025-04-05 10:33:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,55 @@
 # Sistema bancário
 ''' 
 crie um menu principal com as opções de funções bancárias e internas
+1° Funcões Bancarias
 
-Para a funções bancárias
-Crie um menu com as funções bancarias como Sacar, Depositar, extrato voltar ao menu principal
+1.1 Crie um menu com as funções bancarias como Sacar, Depositar, extrato voltar ao menu principal
 
-RESTRIÇÕES!!!
-1º O usuario não pode fazer mais que 10 transações bancarias por dia
+1.2° Saque:
+1.1 O usuario não pode fazer mais que 10 transações bancarias por dia
 (Caso o numero de transação seja atingida mostrar mensagem de erro)
 
-2º O usuario não pode sacar valor acima de R$ 500 por vez
-(Mostrar a mensagem de erro, valor não foi permitido por saque)
+1.2.1 O usuario não pode sacar valor acima de R$ 500 por vez
+(Mostrar a mensagem, valor não foi permitido por saque)
 
-3º O usuario pode sacar no maximo R$1500 por dia
-(Mostrar a mensagem de erro, valor de saque diario atingido)
+1.2.2 usuario pode sacar no maximo R$1500 por dia
+(Mostrar a mensagem, valor de saque diario atingido)
 
-4º O usuario não poderá sacar valores negativo
-(Mostrar a mensagem de erro, valor não permitido)
+1.2.3 usuario não poderá sacar valores negativo
+(Mostrar a mensagem, valor não permitido)
 
-5º Na função deposito, não será permitido deposito com valores negativo
-(Mostrar a mensagem de erro, valor não permitido)
+1.2.4 Em saque realizado, mostrar dados como saldo atual, data e hora do saque
 
-6º Na função Saldo, mostrar o valor em conta
+1.2 Depósito 
+1.2.1 Na função deposito, não será permitido deposito com valores negativo
+(Mostrar a mensagem, valor não permitido)
+
+1.2.2 Não permitir do pedosito caso o numero de transação diária tenha atingida 
+(Mostrar a mensagem, Transação diaria ja atingida)
+
+1.2.3 Em depósito concluido, mostrar mensagem com o valor do depósito, 
+saldo atual em conta, Data e hora
+
+1.3 Saldo
+1.3.1 Na função Saldo, mostrar o valor em conta
 ( Mostrar saldo com data e horario em que foi solicitado)
 
-7º na função extrato, mostrar detalhadamente todas as transações
+1.4 Extrato
+1.4.1 na função extrato, mostrar detalhadamente todas as transações
 ( Mostrar se foi depositado ou sacado, valor, data e hora)
 
-Para funções externas
-Crie um menu com as funções de cadastrar novo usuario, listar usuarios e voltar ao menu principal
+2° Funcões internas
 
-1º Na função cadastrar novos usuarios
-validar com o CPF
-Caso CPF ja esteja cadastrado mostrar mensagem de erro (dados ja cadastrado)
-mostrar todos os dados do usuario que tenha esse CPF
+2.1 Crie um menu com as funções de cadastrar novo usuario, listar usuarios e voltar ao menu principal
+
+2.1.1 Na função cadastrar novos usuarios, validar dados com o CPF
+Caso CPF ja esteja cadastrado mostrar mensagem (dados ja cadastrado)
+oferecer a opção de atualizar cadastro
+
+2.1.2 mostrar todos os dados do usuario que tenha esse CPF
 cadastrar nome, email, telefone e endereço
 
-2º criar a função de volTar ao menu principal
+2.1.3 criar a função de volTar ao menu principal
 
 '''
 from funcao import main

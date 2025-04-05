d742e5b5296315e3a59f0eb4807f4f710fb3410e
Matheus Gonçalves De Britto <94484304+omatheusbritto@users.noreply.github.com>
2025-04-05 10:14:33 -0300

    Update classe.py

--- classe.py
+++ classe.py
@@ -6,17 +6,34 @@ class Operacao:
         self.transacao = 0
 
 class Usuario:
-    def __init__(self):
-        self.nome = str('')
-        self.email = str('')
-        self.cpf = str('')
-        self.telefone = int('')
+    def __init__(self,cpf,email,cpf,telefone,registro):
+        self.nome = nome
+        self.email = email
+        self.cpf = cpf
+        self.telefone = telefone
+        self.registro = {}
+        self.confirmar = confirmar
+        if confirmar =="S":
+            self.registro = {'CPF: ':cpf,
+                             'Nome: ':nome,
+                             'E-mail: ': email,
+                             'Telefone: ':telefone}
 
 class Endereco:
-    def __init__(self):
-        self.cep = int()
-        self.logradouro = str('')
-        self.numero = int('')
-        self.cidade = str('')
-        self.estado = str('')
-        self.pais = str('')
+    def __init__(self,confirmar, cep,logradouro,numero,cidade,estado,pais,registro):
+        self.cep = cep
+        self.logradouro = logradouro
+        self.numero = numero
+        self.cidade = cidade
+        self.estado = estado
+        self.pais = pais
+        self.confirmar = confirmar
+        if confirmar =="S"
+            self.registro.apeend(
+                                 {'CEP: ':cep,
+                                  'Logradouro: ', logradouro,
+                                  'Numero: ':numero,
+                                  'Cidade: ':cidade,
+                                  'Estado: ':estado,
+                                  'País: ':pais}
+        
